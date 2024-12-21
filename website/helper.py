fdb238c44aa45a03028edb840820f74719129c0b
TKatana <turjohalder41@gmail.com>
2024-12-21 13:22:02 +0600

    sql added
    this is for assignment submission

--- website/helper.py
+++ website/helper.py
@@ -20,8 +20,9 @@ def check_is_float_and_convert(str):
 def upload_image_to_imgbb(image_base64):
 
     IMG_BB_URL = "https://api.imgbb.com/1/upload"
-    EXPIRATION = "7872048" # 1 month = 2624016 secs, 1 day = 86400
-    api_key = get_imgbb_api_key()
+    # EXPIRATION = "7872048" # 1 month = 2624016 secs, 1 day = 86400
+    api_key = 'e5c6d0e49b3f16b33a6cb2eb017bc11e'
+        #Enter your ImageBB API Key Here^
     payload = {
         "key": api_key,
         "image": image_base64
